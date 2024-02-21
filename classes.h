834472ea21b30c7a62e8178a97c236bbea4636b8
Wislern <wislerni@oregonstate.edu>
2024-02-20 18:03:55 -0800

    update

--- classes.h
+++ classes.h
@@ -48,8 +48,9 @@ private:
     vector<int> blockDirectory; // Map the least-significant-bits of h(id) to a bucket location in EmployeeIndex (e.g., the jth bucket)
                                 // can scan to correct bucket using j*BLOCK_SIZE as offset (using seek function)
 								// can initialize to a size of 256 (assume that we will never have more than 256 regular (i.e., non-overflow) buckets)
+    vector<float> average;
     int n;  // The number of indexes in blockDirectory currently being used
-    int i;	// The number of least-significant-bits of h(id) to check. Will need to increase i once n > 2^i
+    int bits;	// The number of least-significant-bits of h(id) to check. Will need to increase i once n > 2^i
     int numRecords;    // Records currently in index. Used to test whether to increase n
     int nextFreeBlock; // Next place to write a bucket. Should increment it by BLOCK_SIZE whenever a bucket is written to EmployeeIndex
     string fName;      // Name of index file
@@ -59,66 +60,95 @@ private:
         int remainder = record.id % 216;
 
         string binary_remainder = bitset<8>(remainder).to_string();
-        string least_significant_bits = binary_remainder.substr(binary_remainder.length()-i);
+        string least_significant_bits = binary_remainder.substr(binary_remainder.length()-bits);
 
-        fstream file(fName, ios::app | ios::binary);
+        fstream file(fName, fstream::in | fstream::out | ios::binary);
 
         // Add record to the index in the correct block, creating a overflow block if necessary
         for (int i = 0; i < blockDirectory.size(); ++i) {
             if (blockDirectory[i] == atoi(least_significant_bits.c_str())){
-                // Seek in file to beginging of hashed page
+                // Seek in file to beginning of hashed page
                 int startOfPage = i * BLOCK_SIZE + 1;
                 file.seekp(startOfPage);
-
+                int char1 = file.peek();
                 // See if page is empty
-                bool isEmpty = (file && file.gcount() == 0);
+                bool isEmpty = ((char1 != 48  && char1 != 49));
 
                 // If page is empty, write record to the beginging of the page
                 if (isEmpty){
+                    file.close();
+                    fstream file(fName, fstream::in | fstream::out | ios::binary);
+                    file.seekp(startOfPage);
                     file.write(binary_remainder.c_str(), sizeof(binary_remainder));
+                    if(startOfPage == 1)
+                    {
+                        average[0] += sizeof(binary_remainder);
+                    }
+                    else
+                    {
+                        average[(startOfPage-1)/BLOCK_SIZE] += sizeof(binary_remainder);
+                    }
                 }
-
                 // If page is not empty, loop to the next available space in memory
                 else{
-                    while (!isEmpty) {
+                    int tally = 0;
+                    bool isEmpty2 = false;
+                    while (!isEmpty2) {
+                        tally++;
                         // Read data at current position
-                        char buffer[256];
-                        file.read(buffer, sizeof(buffer));
+                        char char2 = file.peek();
+                        // See if page is empty
+                        isEmpty2 = (char2 == '\000');
         
                         // Check if read was successful and if data is empty
-                        isEmpty = file && file.gcount() == 0;
+                        //isEmpty = file && file.gcount() == 0;
         
                         // If not empty, seek to next location
-                        if (!isEmpty) {
-                            startOfPage += sizeof(buffer);
+                        if (!isEmpty2) {
+                            startOfPage += sizeof(char2);
                             file.seekp(startOfPage);
                         }
                     }
 
                     // If there's no room on page, create overflow page
-                    if ( ((i * BLOCK_SIZE + 1) + sizeof(binary_remainder)) > ((i * BLOCK_SIZE + 1) + 4096) ){
-                        // TO DO
+                    if (average[startOfPage-1/BLOCK_SIZE]/BLOCK_SIZE >= 0.7){
                         // increase n; increase i (if necessary); place records in the new bucket that may have been originally misplaced due to a bit flip
                         // Will need to increase i once n > 2^i
+                        n+=1;
+                        average.push_back(sizeof(binary_remainder)/BLOCK_SIZE);
+                        if (n >= pow(2,i)+1)
+                        {
+                            bits+=1;
+                            for (int i=0; i< pow(2,bits); i++){
+                                blockDirectory[i] = atoi(bitset<8>(i).to_string().c_str());
+                            }
+                            //TODO: rearrange after i increases
+                        }
+                        
                     }
-
+                file.close();
+                fstream file(fName, fstream::in | fstream::out | ios::binary);
+                file.seekp(startOfPage+tally);
                 file.write(binary_remainder.c_str(), sizeof(binary_remainder));
+                average[(startOfPage-1)/BLOCK_SIZE]+=sizeof(binary_remainder);
                 numRecords++;
             }
         }
+
     }
+    file.close();
     }
 
 public:
     LinearHashIndex(string indexFileName) {
         n = 4; // Start with 4 buckets in index
-        i = 2; // Need 2 bits to address 4 buckets
+        bits = 2; // Need 2 bits to address 4 buckets
         numRecords = 0;
         nextFreeBlock = 0;
         fName = indexFileName;
 
         // Create your EmployeeIndex file and write out the initial 4 buckets
-        ofstream file(fName, ios::app);
+        fstream file(fName, ios::app);
         if (!file.is_open()) {
             cerr << "Error creating file: " << fName << endl;
             exit(EXIT_FAILURE);
@@ -132,6 +162,12 @@ public:
         for(int i=4;i<blockDirectory.size();i++)
         {
             blockDirectory[i]=2;
+        }
+
+        average.resize(4);
+        for(int i=0;i<average.size();i++)
+        {
+            average[i]=0;
         }
 
         // make sure to account for the created buckets by incrementing nextFreeBlock appropriately
